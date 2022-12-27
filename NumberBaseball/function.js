c3adc8b304252f2667f780e8a40a259303fe3ced
ymw0407 <yunminwo1211@gmail.com>
2022-12-28 01:26:05 +0900

    :sparkles: feat : 정답 제공 함수 randomAnswer 추가

--- NumberBaseball/function.js
+++ NumberBaseball/function.js
@@ -1,12 +1,37 @@
-function result() {
-    var list = getList();
-}
-
-const getList = () => {
-    let ls = []
-    for(i=1; i<=4; i++){
-        ls.push(document.getElementById("list" + i).childNodes.item(0).getAttribute("id"));
+const randomAnswer = () => {
+    const ANSWER = [];
+    let answer;
+    for (i = 1; i <= 4; i++) {
+        answer = parseInt(Math.random() * 9) + 1;
+        while (isAnswerDuplicate(answer, ANSWER)) {
+            answer = parseInt(Math.random() * 9) + 1
+        }
+        ANSWER.push(answer);
     }
-    console.log(ls)
-    return ls
+    console.log(ANSWER);
+};
+
+const isAnswerDuplicate = (answer, ANSWER) => {
+    return ANSWER.includes(answer);
+};
+
+//-------------------- main code --------------------
+randomAnswer();
+function result() {
+    var ballList = getBallList();
 }
+//-------------------- main code --------------------
+
+const getBallList = () => {
+    let ballList = [];
+    for (i = 1; i <= 4; i++) {
+        ballList.push(
+            document
+                .getElementById("list" + i)
+                .childNodes.item(0)
+                .getAttribute("id")
+        );
+    }
+    console.log("[getBallList] ", ballList);
+    return ballList;
+};
